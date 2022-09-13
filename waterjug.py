a28b17cb61bea9d757c0e46bb1ff7d7dece99e70
bhargavsai19 <113325286+bhargavsai19@users.noreply.github.com>
2022-09-13 10:18:31 +0530

    Add files via upload

---
+++ waterjug.py
@@ -0,0 +1,14 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==================================================== RESTART: C:/Users/BHARGAV SAI/AppData/Local/Programs/Python/Python310/puzzle.py ===================================================
+Path from initial state to solution state ::
+( 0 , 0 )
+( 0 , 3 )
+( 4 , 0 )
+( 4 , 3 )
+( 3 , 0 )
+( 1 , 3 )
+( 3 , 3 )
+( 4 , 2 )
+( 0 , 2 )
